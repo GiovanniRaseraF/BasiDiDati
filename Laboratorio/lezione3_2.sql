33f61f969c596c86ea1b1a5b94d0cb79b0e6259f
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2021-02-06 16:01:13 +0100

    Aggiunta delle user defined functions

--- Laboratorio/lezione3_2.sql
+++ Laboratorio/lezione3_2.sql
@@ -41,4 +41,79 @@ select somma_prodotto(3, 4);
 
 select * from somma_prodotto(3, 4);
 
---
+--Posso create dei tipi di ritorno
+create type somma_prod as (somma int, prod int);
+
+create or replace function somma_prodotto(x int, y int)
+returns somma_prod 
+language plpgsql as $$
+    declare 
+        risultato record;
+    begin 
+        risultato := (x+y, x*y);
+        return risultato;
+    end;
+$$;
+
+--Valore di ritorno come tabella
+create or replace function donne_dip(nome_dip varchar(20)) 
+returns table (
+    cognome nomi_persona,
+    iniziale iniziali_persona,
+    nome nomi_persona
+)
+language plpgsql as $$
+    begin 
+        return query 
+        select I.cognome, I.iniziale, I.nome 
+        from Dipartimento D, Impiegato I 
+        where I.dip = D.dnumero 
+            and D.dnome = nome_dip 
+            and I.sesso = 'F';
+    end;
+$$;
+
+select * from donne_dip('Dipartimento 1');
+
+--Un impiegato e il suo supervisore devono afferire allo stesso
+-- dipartimento
+--    verrà chiamata ogni volta che faccio un inserimento nella
+--    tabella
+create or replace function 
+controlla_supervisore(imp cf_persona)
+returns boolean language plpgsql as
+$$
+    declare 
+        ok boolean; 
+    begin 
+        raise notice 'Controllo supervisore di %', imp;
+        
+        select I.dip = S.dip into ok 
+        from Impiegato I join Impiegato S on I.cf = imp 
+        where I.supervisore = S.cf;
+
+        return ok;
+    end;
+$$;
+--controllo il vincolo ogni volta che aggiungo una riga
+alter table Impiegato add constraint supervisione 
+check (controlla_supervisore(cf));
+
+--Esercizio
+
+--Si definisca una funzione che, ricevuto in input un mid,
+-- restituisca il suo ratings medio
+create or replace function rating_medio(mid int)
+returns number 
+language plpgsql as 
+$$ 
+    declare 
+        r_medio number;
+    begin 
+        select avg(stars) to r_medio 
+        from rating 
+        where rating.mid = mid;
+
+        return r_medio;
+    end;
+$$;
