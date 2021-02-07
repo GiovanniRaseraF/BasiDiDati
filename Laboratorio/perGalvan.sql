25dd5072e19c0375875dba0cd1364151ee3830fb
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2021-02-07 23:18:11 +0100

    Per galvan

---
+++ Laboratorio/perGalvan.sql
@@ -0,0 +1,37 @@
+--Esercizio per Galvan
+create schema if not exists films;
+set search_path to films;
+create domain nomi as varchar(20);
+
+--Tabella film
+create table film(
+    codicefilm      int,
+    titolo          nomi,
+    regista         nomi,
+    anno            int,
+    --vincoli di integrità
+    primary key(codicefilm),
+    constraint anno_creazione  check (anno > 1800);
+);
+
+create table attore(
+    codiceattore    int,
+    cognome         nomi,
+    nome            nomi,
+    sesso           char,
+    datanascita     date,
+    nazionalita     nomi,
+    --vincoli di integrità
+    primary key(codiceattore),
+    constraint sesso_possibile check (sesso in ('m', 'f'));  
+);
+
+create table interpretazione(
+    film            int,
+    attore          int,
+    personaggio     nomi,
+    --vincoli di integrità
+    primary key(film, attore),
+    foreign key(attore) references attore.codiceattore,
+    foreign key(film)   references film.codicefilm
+);
